71f9aefbe61cdd6db774c760828b3cf1575f5d38
Martin <martinbbanchero@gmail.com>
2023-12-04 20:55:17 +0100

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,11 +24,11 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+#html_theme = "sphinx_rtd_theme"
 #html_theme = 'alabaster'
 html_static_path = ['_static']
 extensions = ["myst_parser"]
-#html_theme = 'furo'
+html_theme = 'furo'
 html_css_files = [
     'css/custom.css',
 ]
